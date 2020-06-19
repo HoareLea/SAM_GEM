80aab551c9d7cc4a85dc61741d5c7cd999f35fac
Michal Dengusiak <tassimulation@gmail.com>
2020-06-19 15:57:03 +0100

    Initial commit

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Convert/ToGrasshopper/GH_Surface.cs
@@ -0,0 +1,23 @@
+﻿using Grasshopper.Kernel.Types;
+using SAM.Geometry.Grasshopper;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public static partial class Convert
+    {
+        public static GH_Surface ToGrasshopper(this Panel panel)
+        {
+            return panel.PlanarBoundary3D.ToGrasshopper();
+        }
+
+        public static GH_Surface ToGrasshopper(this PlanarBoundary3D planarBoundary3D)
+        {
+            return new GH_Surface(planarBoundary3D.ToRhino());
+        }
+
+        public static GH_Surface ToGrasshopper(this Aperture aperture)
+        {
+            return new GH_Surface(aperture.GetFace3D().ToRhino_Brep());
+        }
+    }
+}
